9b33df750478833b461dad6658e1555eeca4daf1
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-07-04 08:31:19 +0200

    Update Marketplace.js

--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -1,10 +1,1 @@
-import React from 'react';
 
-export default function Marketplace() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Marketplace</h1>
-      <p>Welcome to the Marketplace! Products will show here.</p>
-    </div>
-  );
-}
