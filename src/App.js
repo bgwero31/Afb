db954b2edcf14ef409414b5c5ab3ae4a68cfc13f
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-07-04 07:24:45 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Marketplace from './components/Marketplace';
+import Chatroom from './components/Chatroom';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ padding: '10px', background: '#333' }}>
+        <Link to="/" style={{ color: '#fff', marginRight: '10px' }}>Marketplace</Link>
+        <Link to="/chat" style={{ color: '#fff', marginRight: '10px' }}>Chatroom</Link>
+        <Link to="/profile" style={{ color: '#fff' }}>Profile</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Marketplace />} />
+        <Route path="/chat" element={<Chatroom />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
